caf5fd361f2512c7f414d66c436fe00533b5e202
Swamy Kankala <swamykankala@Swamys-MacBook-Pro.local>
2022-06-06 19:47:43 -0700

    feat: increment decrement counter

---
+++ increment-decrement/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import { useReducer } from "react";
+
+const initialState = { count: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <center>
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+    </center>
+  );
+}
+
+export default App;
